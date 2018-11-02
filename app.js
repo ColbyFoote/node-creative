14d1a31f0e7d9f0c530bb34546388aede246e51c
Colby <2feet4me@gmail.com>
2018-11-02 18:54:56 +0000

    done

--- app.js
+++ app.js
@@ -14,25 +14,34 @@ io.on('connection', function(socket) {
   console.log('a user connected');
   socket.on("join", function(name) {
     console.log(name)
-    socket.userName = name;
-    
-    socket.emit("userUpdate", "You have connected to the server.")
-    socket.emit("getMessages", messages);
-    socket.emit("getUsers", people);
-    people.push(name);
-    io.emit("newUser", name);
-    var event = socket.userName + " has joined the chat."
-    events.push(event)
-    io.emit("userUpdate", event)
+    if (people.filter(el => el == name).length > 0) {
+      socket.emit("duplicateUser", name);
+    }
+    else {
+      socket.emit("validUser", name);
+      socket.userName = name;
+      socket.emit("userUpdate", "You have connected to the server.")
+      socket.emit("getMessages", messages);
+      socket.emit("getUsers", people);
+      people.push(name);
+      io.emit("newUser", name);
+      var event = socket.userName + " has joined the chat."
+      events.push(event)
+      io.emit("userUpdate", event)
+    }
   })
   socket.on("newMessage", function(msg) {
     console.log(msg);
-    var newMessage = {user:socket.userName, message:msg}
+    var newMessage = { user: socket.userName, message: msg.message, date: msg.date }
     messages.push(newMessage);
     io.emit("newMessage", newMessage);
   })
   socket.on('disconnect', function() {
     console.log('user disconnected');
+    if (socket.userName) {
+      people = people.filter(el => el != socket.userName);
+      io.emit("leave", socket.userName);
+    }
   });
 });
 
